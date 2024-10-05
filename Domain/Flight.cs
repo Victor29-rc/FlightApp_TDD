b5a648539de08dc0732842636fae42d08b11636b
Victor Rosario <manuel_victor_rc@hotmail.com>
2024-10-05 13:44:02 -0400

    First commit - Flight Application with its TESTS

---
+++ Domain/Flight.cs
@@ -0,0 +1,38 @@
+﻿
+namespace Domain
+{
+    public class Flight
+    {
+        List<Booking> _bookingsLists = new();
+        public IEnumerable<Booking> BookingList => _bookingsLists;
+        public int RemainingNumbersOfSeats { get; set; }
+        public Flight(int seatCapacity)
+        {
+            RemainingNumbersOfSeats = seatCapacity;
+        }
+
+        public object? Book(string passengerEmail, int numberOfSeats)
+        {
+            if (RemainingNumbersOfSeats - numberOfSeats < 0) 
+            { 
+                return new OverbookingError();
+            }
+
+            RemainingNumbersOfSeats -= numberOfSeats;
+
+            _bookingsLists.Add(new Booking(passengerEmail, numberOfSeats));
+
+            return null;
+        }
+
+        public object? CancelBooking(string passengerEmail, int numberOfSeats)
+        {
+            if(!_bookingsLists.Any(booking => booking.Email == passengerEmail))
+                return new BookingNotFoundError();
+            
+            RemainingNumbersOfSeats += numberOfSeats;
+
+            return null;
+        }
+    }
+}
